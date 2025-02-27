27e2a2886c93697e1f1874c983a55b2368e6484a
Fajeeza Feroz <fajeezaf@gmail.com>
2025-02-27 21:27:45 +0530

    redux to Redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from './Redux/store';
 import App from './App';
 import './index.css'
 import { ToastContainer } from 'react-toastify';
